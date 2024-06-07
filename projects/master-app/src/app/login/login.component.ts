e17699c60733958ede3ccb2f83eeaf111c42698c
LHTrungSkySP <lehoangtrung038582@gmail.com>
2024-06-08 03:51:27 +0700

    a

--- projects/master-app/src/app/login/login.component.ts
+++ projects/master-app/src/app/login/login.component.ts
@@ -5,48 +5,56 @@ import { DragDropModule } from 'primeng/dragdrop';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet,DragDropModule],
+  imports: [CommonModule, RouterLink, RouterOutlet, DragDropModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  fullList = [
-    { id: '1', name: 'phan tu 1' },
-    { id: '2', name: 'phan tu 2' }
-  ]
-  targetList = {
-    id: 0,
-    items: [{}]
-  };
-  draggedProduct: any[] = [];
-  dragStart(product: any) {
-    this.draggedProduct = product;
-    console.log(this.draggedProduct);
-    
+  listSource: Container = new Container(
+    1,
+    [
+      { id: '1', name: 'phan tu 1'},
+      { id: '2', name: 'phan tu 2'},
+      { id: '3', name: 'phan tu 3'},
+      { id: '4', name: 'phan tu 4'},
+    ]
+  )
+  listTarget: Container = new Container(2, [])
+  listTargetB: Container = new Container(3, [])
+
+  listManage: Container[] = [];
+  listTemp: Container | null = null; // data đang được kéo thả
+  constructor() {
+    this.listManage = [this.listSource, this.listTarget, this.listTargetB]
+  }
+  dragStart(item: any, id: number) {
+    this.listTemp = new Container(id, [item]);
   }
 
-  drop(target: any[], source: any[]) {
-    if (this.draggedProduct) {
-      let draggedProductIndex = this.findIndex(this.draggedProduct,source);
-      target.push(this.draggedProduct);
-      source.splice(draggedProductIndex, 1)
-      this.draggedProduct = [];
-      
+  drop(id: number, item: any = {}, itemDraping: any) {
+    if (this.listTemp) {
+      let target = this.listManage.find((e: Container) => e.id === id); // list được thả data
+      let source = this.listManage.find((e: Container) => e.id === itemDraping.id); // list chứa data
+
+      let indexItemSource = source?.items.indexOf(itemDraping.items[0]) ?? 0; // index của item đang draping trong source 
+      let indexItemTarget = target?.items.indexOf(item) ?? 0; // index của item bị thay thế trong target 
+
+      source?.items.splice(indexItemSource, 1);
+      target?.items.splice(indexItemTarget, 0, itemDraping.items[0])
+      this.listTemp = null;
     }
   }
 
   dragEnd() {
-    this.draggedProduct = [];
+    this.listTemp = null;
   }
 
-  findIndex(product: any, list: any[]) {
-    let index = -1;
-    for (let i = 0; i < list.length; i++) {
-      if (product.id === list[i].id) {
-        index = i;
-        break;
-      }
-    }
-    return index;
+}
+class Container {
+  id: number;
+  items: any[];
+  constructor(id: number, items: any[]) {
+    this.id = id;
+    this.items = items;
   }
 }
